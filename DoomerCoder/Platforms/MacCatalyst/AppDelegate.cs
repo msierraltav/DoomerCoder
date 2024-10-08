78932ff7133b90f6d5c1b12a9460b61d8b09eda2
Mitch <115440917+msierraltav@users.noreply.github.com>
2024-10-08 00:29:20 -0600

    Add project files.

---
+++ DoomerCoder/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DoomerCoder
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
